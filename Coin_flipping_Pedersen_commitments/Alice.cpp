c440f2ca25c3d7dc1e9dea91f722d62e13c007ec
wind123mountain <57493825+wind123mountain@users.noreply.github.com>
2021-07-14 23:44:48 +0700

    Update Alice.cpp

--- Coin_flipping_Pedersen_commitments/Alice.cpp
+++ Coin_flipping_Pedersen_commitments/Alice.cpp
@@ -17,15 +17,13 @@ char inp[MAX_SIZE]{0};
 
 void connection(){
     // Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-	{
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
 		perror("socket failed");
 		exit(EXIT_FAILURE);
 	}
 
 	// Forcefully attaching socket to the port 8080
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-	{
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
 		perror("setsockopt");
 		exit(EXIT_FAILURE);
 	}
@@ -34,18 +32,15 @@ void connection(){
 	address.sin_port = htons( PORT );
 
 	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
-	{
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0){
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
-	if (listen(server_fd, 3) < 0)
-	{
+	if (listen(server_fd, 3) < 0){
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
-	{
+	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0){
 		perror("accept");
 		exit(EXIT_FAILURE);
 	}
@@ -54,10 +49,10 @@ void connection(){
 int main(int argc, char const *argv[]){
 
 	mpz_t p, q, g, h;
-    mpz_init(p);
-    mpz_init(q);
-    mpz_init(g);
-    mpz_init(h);
+	mpz_init(p);
+	mpz_init(q);
+	mpz_init(g);
+	mpz_init(h);
 
 	char ans[10] ={0};
 	char *mess = "1";
@@ -88,35 +83,34 @@ int main(int argc, char const *argv[]){
 
 	cout<<"\n================================ PUBLIC ================================\n";
 	cout<<"p = ";
-    gmp_printf("%Zd\n\n", p);
-    cout<<"q = ";
-    gmp_printf("%Zd\n\n", q);
-    cout<<"g = ";
-    gmp_printf("%Zd\n\n", g);
-    cout<<"h = ";
-    gmp_printf("%Zd\n", h);
+	gmp_printf("%Zd\n\n", p);
+	cout<<"q = ";
+	gmp_printf("%Zd\n\n", q);
+	cout<<"g = ";
+	gmp_printf("%Zd\n\n", g);
+	cout<<"h = ";
+	gmp_printf("%Zd\n", h);
 
 
 	mpz_t com, r, x;
-    mpz_init(com);
-    mpz_init(r);
-    mpz_init(x);
-
-    cout<<"\n================================ COMMIT ================================\n";
-    cout<<"Enter value of x (belong to Zq): ";
-    //cin.ignore();
-    cin.getline(inp, MAX_SIZE);
-    mpz_set_str(x, inp, 10);
-
-    commit(com, p, q, g, h, x, r);
-    gmp_printf("\nr = %Zd\n\n", r);
-    gmp_printf("com = %Zd\n\n", com);
-
-    mpz_get_str(output, 10, com);
-    fout.open("Bob.txt", ios::out | ios:: trunc);
-    fout<<output;
-    fout<<"\n";
-    fout.close();
+	mpz_init(com);
+	mpz_init(r);
+	mpz_init(x);
+	
+	cout<<"\n================================ COMMIT ================================\n";
+	cout<<"Enter value of x (belong to Zq): ";
+	cin.getline(inp, MAX_SIZE);
+	mpz_set_str(x, inp, 10);
+	
+	commit(com, p, q, g, h, x, r);
+	gmp_printf("\nr = %Zd\n\n", r);
+	gmp_printf("com = %Zd\n\n", com);
+	
+	mpz_get_str(output, 10, com);
+	fout.open("Bob.txt", ios::out | ios:: trunc);
+	fout<<output;
+	fout<<"\n";
+	fout.close();
 
 	send(new_socket, mess, strlen(mess), 0);
 	printf("Message sent!\n");
@@ -130,17 +124,17 @@ int main(int argc, char const *argv[]){
 	fout.open("Bob.txt", ios::out | ios:: trunc);
 	mpz_get_str(output, 10, x);
 	fout<<output;
-    fout<<"\n";
+	fout<<"\n";
 
 	mpz_get_str(output, 10, r);
 	fout<<output;
-    fout<<"\n";
+	fout<<"\n";
 
 	fout.close();
 	send(new_socket, mess, strlen(mess), 0);
 	printf("Message sent!\n\n");
-
-    mpz_clears(p, q, g, h, com, r, x, NULL);
+	
+	mpz_clears(p, q, g, h, com, r, x, NULL);
 
 	return 0;
 }
